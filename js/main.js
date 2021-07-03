2b58666b1c76d64c822c58c2e842ad86ad760db9
unknown <abdosalama25802030@gmail.com>
2021-07-03 18:25:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut();
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
